07dbaa6fa76c90f2ab81fba8419b94b53490d0a5
choa293 <choa293@newschool.edu>
2021-05-01 22:52:24 -0700

    progress

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+  var myArray = ['#FFE895', '#82C0FA', '#FFB1F7', '#B0FB95'];
+  var rand = myArray[Math.floor(Math.random() * myArray.length)];
+  document.body.style.backgroundColor = rand;
+
+
+  let input = document.querySelector("#input")
+  document.getElementById("input").addEventListener('onkeypress', logKey);
+
+  function showText() {
+ 
+}
+
+
+  detect when pressing a key 
+  grab value of input and set it equal to set the text into the result box 
+
+ 
+
+ //make button that triggers function that changes the css 
